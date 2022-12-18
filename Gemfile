dea700038d30b58a85f284e598329a71626b5864
root <root@DESKTOP-1LOTRL1.localdomain>
2022-12-18 12:25:29 +0200

    Internationalization

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "jbuilder"
 
 gem "bcrypt", "~> 3.1.7"
 
+gem 'i18n-js', "~> 3.0.0"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "i18n"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +11,7 @@ module Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.middleware.use I18n::JS::Middleware
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
